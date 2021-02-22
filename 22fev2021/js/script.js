2b3e3d546a02fdf7e06841495d9c90ec3605f922
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-02-22 15:08:57 +0100

    Ajout de la fonction pour réinitialiser le formulaire

--- 22fev2021/js/script.js
+++ 22fev2021/js/script.js
@@ -1,7 +1,7 @@
 /* 
-Créer une fonction pour exécuter une requête ASYNC
+Créer une fonction pour utiliser la méthode HTTP GET
 */
-    const getData = (type, id = null) => {
+    const getData = (type, id = null, update = false) => {
         // Définir le endpoint
         let endpoint = null;
 
@@ -28,7 +28,7 @@ Créer une fonction pour exécuter une requête ASYNC
                 // Tester le paramêtre id
                 id === null
                 ? displayPosts('#postList', jsonData)
-                : displaySinglePost('#mainContent', jsonData)
+                : displaySinglePost('#mainContent', jsonData, update)
             }
             else{
                 console.log(type)
@@ -64,6 +64,72 @@ Créer une fonction pour utiliser la méthode HTTP POST
             }
         })
         .then( jsonData => {
+            // Réinitialiser le formulaire
+            resetForm();
+
+            console.log(jsonData)
+        })
+        .catch( error => {
+            console.log(error)
+        });
+    }
+//
+
+/* 
+Créer une fonction pour utiliser la méthode HTTP PUT
+*/
+    const putData = (endpoint, id, data) => {
+        // Mise en place d'une fonction Fetch en PUT
+        fetch(`http://localhost:3000/${endpoint}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then( reponse => {
+            // Vérifier l'état de la réponse
+            if( reponse.ok === false ){
+                // Afficher l'erreur dans la console
+                console.log(reponse)
+            }
+            else{
+                // Extraire les données JSON de la réponse
+                return reponse.json()
+            }
+        })
+        .then( jsonData => {
+            // Réinitialiser le formulaire
+            resetForm();
+            
+            console.log(jsonData)
+        })
+        .catch( error => {
+            console.log(error)
+        });
+    }
+//
+
+/* 
+Créer une fonction pour utiliser la méthode HTTP DELETE
+*/
+    const deletePost = (endpoint, id) => {
+        // Mise en place d'une fonction Fetch en POST
+        fetch(`http://localhost:3000/${endpoint}/${id}`, {
+            method: 'DELETE'
+        })
+        .then( reponse => {
+            // Vérifier l'état de la réponse
+            if( reponse.ok === false ){
+                // Afficher l'erreur dans la console
+                console.log(reponse)
+            }
+            else{
+                // Extraire les données JSON de la réponse
+                return reponse.json()
+            }
+        })
+        .then( jsonData => {
             console.log(jsonData)
         })
         .catch( error => {
@@ -82,21 +148,23 @@ Créer une fonction pour afficher la liste d'articles
             document.querySelector(htmlTag).innerHTML += `
                 <li>
                     <a href="${item.id}" class="postLink">${item.title}</a>
+                    <button type="button" class="deleteBtn" data-id="${item.id}"><i class="fas fa-times"></i></button>
+                    <button type="button" class="updateBtn" data-id="${item.id}"><i class="fas fa-pen"></i></button>
                 </li>
             `;
         }
 
         // Capter le click sur les balise ".postLink"
-        getLinkClick('.postLink')
+        getLinkClick('.postLink', '.deleteBtn', '.updateBtn')
     }
 //
 
 /* 
 Créer une fonction pour capter le click sur les balise ".postLink"
 */
-    const getLinkClick = (htmlTags) => {
+    const getLinkClick = (linkTags, deleteTags, updateTags) => {
         // Faire une boucle sur la collection de liens
-        for( let item of  document.querySelectorAll(htmlTags)){
+        for( let item of  document.querySelectorAll(linkTags)){
             // Capter le click sur chaque balise ".postLink"
             item.addEventListener('click', (event) => {
                 // Bloquer le comportement par défaut de la balise a
@@ -106,18 +174,51 @@ Créer une fonction pour capter le click sur les balise ".postLink"
                 getData('posts', event.target.getAttribute('href'));
             })
         }
+
+        // Faire une boucle sur la collection de boutons
+        for( let item of  document.querySelectorAll(deleteTags)){
+            // Capter le click sur chaque balise ".postLink"
+            item.addEventListener('click', (event) => {
+                // Lancer la requête HTTP DELETE
+                deletePost('posts', item.getAttribute('data-id'))
+            })
+        }
+
+        // Faire une boucle sur la collection de boutons
+        for( let item of  document.querySelectorAll(updateTags)){
+            // Capter le click sur chaque balise ".postLink"
+            item.addEventListener('click', (event) => {
+                // Lancer la requête HTTP PUT
+                getData('posts', item.getAttribute('data-id'), true );
+            })
+        }
     }
 //
 
 /* 
 Créer une fonction pour afficher le contenu d'un article
 */
-    const displaySinglePost = ( htmlTag, data ) => {
-        document.querySelector(htmlTag).innerHTML = `
-            <h1>${data.title}</h1>
-            <p>${data.content}</p>
-            <p>Par ${data.author}</p>
-        `;
+    const displaySinglePost = ( htmlTag, data, update ) => {
+        // Vérifier le paramêtre update
+        if( update === false ){
+            // Afficher l'article
+            document.querySelector(htmlTag).innerHTML = `
+                <h1>${data.title}</h1>
+                <p>${data.content}</p>
+                <p>Par ${data.author}</p>
+            `;
+        }
+        else{
+            // Mettre à jour les champs du formulaire
+            document.querySelector('#addPost form').setAttribute('method', 'PUT');
+            document.querySelector('[name="title"]').value = data.title;
+            document.querySelector('[name="content"]').value = data.content;
+            document.querySelector('[name="post-id"]').value = data.id;
+
+            // Mettre à jour le bouton du formulaire
+            document.querySelector('[type="submit"]').innerHTML = 'Mettre à jour';
+        }
+        
     }
 //
 
@@ -139,9 +240,31 @@ Créer une fonction pour capter le submit du formulaire
                 author: 'Julien'
             }
 
-            // Utiliser la fonction postData pour ajouter un nouvel article dans la BDD
-            postData('posts', bodyData);
+            // Vérifier la méthode du formulaire
+            if( event.target.getAttribute('method') === 'POST' ){
+                // Utiliser la fonction postData pour ajouter un nouvel article dans la BDD
+                postData('posts', bodyData);
+            }
+            else{
+                // Utiliser la fonction putData pour modifier un article dans la BDD
+                putData('posts', document.querySelector('[name="post-id"]').value, bodyData);
+            }
         })
+    }
+//
+
+/* 
+Créer une méthode pour réinitialiser le formulaire
+*/
+    const resetForm = () => {
+        // Réinitialiser les champs du formulaire
+        document.querySelector('#addPost form').reset();
+
+        // Réinitialiser la méthode du formulaire
+        document.querySelector('#addPost form').setAttribute('method', 'POST');
+
+        // Réinitialiser le bouton du formulaire
+        document.querySelector('[type="submit"]').innerHTML = 'Ajouter'
     }
 //
 
