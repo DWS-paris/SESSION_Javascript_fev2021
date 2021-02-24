d79103e9a81c0d0299c65b6a51e1eeb425b50a0b
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-02-24 10:10:10 +0100

    Modification de l'animation de création de page

---
+++ 24fev2021/js/FETCHclass.js
@@ -0,0 +1,33 @@
+// Définition d'une classe (ES^6)
+class FETCHclass {
+    // Ajouter un constructeur pour injecter des valeurs dans la classe
+    constructor( url, requestType, bodyData = null ){
+        this.url = url;
+        this.requestType = requestType;
+        this.bodyData = bodyData;
+
+        // Vérifier le type de requête pour définir le header
+        if( this.requestType === "GET" ){
+            this.fetchOptions = {
+                method: this.requestType
+            }
+        }
+    }
+
+    // Créer une méthode pour lancer la requête
+    sendRequest(){
+        // Renvoyer une promesse pour gérer une requête ASYNC
+        return new Promise( ( resolve, reject ) => {
+            // Exécuter une requête HTTP en utilisant l'API Fetch
+            fetch(this.url, this.fetchOptions)
+            .then( response => {
+                // Vérifier l'état de la réponse
+                return !response.ok
+                ? reject(response)
+                : response.json();
+            })
+            .then( jsonData => resolve(jsonData) )
+            .catch( error => reject(error) );
+        })
+    }
+}
---
+++ 24fev2021/js/script.js
@@ -0,0 +1,115 @@
+/* 
+Déclarations
+*/
+//
+
+
+/* 
+Fonctions
+*/
+    const displayNav = (htmlTag) => {
+        // Utiliser la classe FETCHclass pour récupérer les données
+        new FETCHclass('http://localhost:3000/nav', 'GET').sendRequest()
+        .then( jsonData => {
+            // Modifier le DOM
+            for( let item of jsonData ){
+                document.querySelector(`${htmlTag} ul`).innerHTML += `
+                    <li>
+                        <a href="${item.content}" class="navLink">
+                            <span class="animAll" style="background:${item.color}"></span>
+                            <b>${item.name}</b>
+                        </a>
+                    </li>
+                `;
+            }
+
+            // initialiser la navigation
+            initNavigation();
+        })
+        .catch( fetchError => {
+            console.log(fetchError);
+        });
+    };
+
+    const initNavigation = () => {
+        // Capter le clic sur le burger menu
+        document.querySelector('#burgerBtn').addEventListener('click', () => {
+            // Ajouter/Supprimer une classe open à la balise #mainNavigation
+            document.querySelector('#mainNavigation').classList.toggle('open');
+        })
+
+        // Capter le clic sur les balise à de la nav
+        for( let item of document.querySelectorAll('.navLink') ){
+            item.addEventListener('click', (event) => {
+                // Bloquer le comportement par défaut de la balise a
+                event.preventDefault();
+
+                // Charger le contenu de la page
+                loadPageContent( item.getAttribute('href') )
+            })
+        }
+
+        // Charger le contenu de la page d'accueil
+        loadPageContent('home-page')
+    }
+
+    const loadPageContent = (content) => {
+        // Afficher le loading
+        document.querySelector('body').classList.add('loading')
+        document.querySelector('#mainNavigation').classList.remove('open')
+        document.querySelector('#mainContent').classList.remove('display')
+
+        // Utiliser la classe FETCHclass pour récupérer les données
+        new FETCHclass(`http://localhost:3000/${content}`, 'GET').sendRequest()
+        .then( jsonData => displayPage(jsonData))
+        .catch( fetchError => {
+            console.log(fetchError);
+        });
+    }
+
+    const displayPage = (jsonData) => {
+        console.log(jsonData)
+
+        
+
+        // Appliquer la couleur de lapage
+        document.querySelector('#mainColor').style.background = jsonData.color
+
+        // Vérifier la section à afficher
+        if( jsonData.section === "homePage" ){
+            document.querySelector('#mainContent').innerHTML = `
+                <div>
+                    <p id="siteTitle">${jsonData.title}</p>
+                    <h1>${jsonData["sub-title"]}</h1>
+                </div>
+            `;
+        }
+
+        setTimeout(() => {
+            // Masquer le loading
+            document.querySelector('body').classList.remove('loading')
+
+            // Afficher le contenu
+            setTimeout(() => {
+                // Ajouter la classe de la page
+                document.querySelector('#mainContent').classList.remove('homePage')
+                document.querySelector('#mainContent').classList.remove('aboutPage')
+                document.querySelector('#mainContent').classList.remove('portfolioPage')
+                document.querySelector('#mainContent').classList.remove('contactsPage')
+                document.querySelector('#mainContent').classList.add(jsonData.section);
+                
+                document.querySelector('#mainContent').classList.add('display')
+            }, 500);
+        }, 500);
+    }
+//
+
+
+/* 
+Attendre le chargement du DOM
+*/
+    document.addEventListener('DOMContentLoaded', async () => {
+        // Afficher la navigation
+        displayNav('#mainNavigation');
+    });
+//
